6a6b49172bcc7cd451a8f4f465d06027fd5f66e8
Ross Schlaikjer <ross@schlaikjer.net>
2014-12-05 15:53:36 -0500

    Add custom css for Not Found page
    Add some custom css rules to the not found page to demo the less system
    
    References #83

--- js/src/pages/not_found.js
+++ js/src/pages/not_found.js
@@ -1,11 +1,26 @@
 /** @jsx React.DOM */
 
-var React = require('react');
+var React = require('react'),
+    mui = require('material-ui');
 
 module.exports = React.createClass({
     render: function() {
         return (
-            <p>This page could not be found</p>
+                <div className="page-not-found-div">
+                    <span className="page-not-found-head">Not Found</span>
+                    <br />
+                    <span className="page-not-found-message">
+                        Shepards are being called in to diagnose the problem.
+                    </span>
+                    <br/>
+                    <div>
+                        <mui.FlatButton
+                            onClick={function(){document.location.href="/#/"}}
+                            label="Get Me Outta Here"
+                            primary={true}
+                        />
+                    </div>
+                </div>
         );
     }
 });
