5399fc9152616b5dfb564c1ab81d1647c9c313da
Connor Blanck <connorblanck@gmail.com>
2014-12-10 13:38:52 -0500

    Adding forgot password

--- js/src/pages/login.js
+++ js/src/pages/login.js
@@ -23,6 +23,33 @@ var Login = React.createClass({
       }
     }
   },
+  handleForgot: function(e) {
+    e.preventDefault();
+    if (!this.state.forgot) {
+      this.state.forgot = true;
+      $('#login-form').append(
+        <form id="forgot-form" className="forgot-form" onSubmit={this.handleForgotSubmit}>
+        <mui.Input type="text"
+             onChange={this.handleChange.bind(this, "forgot_user")}
+             placeholder="Username" name="forgot_user" />
+        <mui.RaisedButton label="submit"/>
+        </form>
+      );
+    }
+  },
+  handleForgotSubmit: function(e) {
+    e.preventDefault();
+    $.ajax({
+      url: '/api/user',
+      method: 'POST',
+      data: {
+        method: 'forgot_password',
+        user: this.state.forgot_user,
+      },
+      dataType: 'json',
+    });
+    $('#forgot-form').append("<p>Password reset has been emailed to you</p>");
+  },
   render: function() {
     if (Session.is_logged_in()) {
       document.location.href="/#/";
@@ -32,15 +59,16 @@ var Login = React.createClass({
         <div className="login-div">
           <h1>Login</h1>
           <form id="login-form" className="login-form" onSubmit={this.handleSubmit}>
-            <mui.Input type="text" 
-                 onChange={this.handleChange.bind(this, "username")} 
+            <mui.Input type="text"
+                 onChange={this.handleChange.bind(this, "username")}
                  placeholder="Username" name="username" />
-            <mui.Input type="password" 
+            <mui.Input type="password"
                  onChange={this.handleChange.bind(this, "password")}
                  placeholder="Password" name="password" />
             <mui.RaisedButton primary={true} label="submit"/>
           </form>
         </div>
+        <mui.RaisedButton label="Forgot password" onClick={this.handleForgot}>
       );
     }
   }
