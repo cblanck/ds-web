f99162f248dae9c3f1810c0052d71efdb4b54cd1
Connor Blanck <connorblanck@gmail.com>
2015-03-05 20:43:51 -0500

    Adding user page as new home page

--- js/src/pages/home.js
+++ js/src/pages/home.js
@@ -1,13 +1,27 @@
 /** @jsx React.DOM */
 
-var React = require('react');
+var Session = require('../util/session.js'),
+    mui = require('material-ui'),
+    React = require('react');
+
 
 var Home = React.createClass({
-    render: function() {
-        return (
-            <p>This is the root page</p>
-        );
+  render: function() {
+    if(Session.is_logged_in()){
+      return (
+        <mui.Paper zdepth={1}>
+          User info goes here
+        </mui.Paper>
+      );
+    } else {
+      return (
+        <div className="home-div">
+          <h1>Join the flock</h1>
+          <img className="flock-image" src="../../../static/ttronslien-9405.jpg"/>
+        </div>
+      );
     }
+  }
 });
 
 module.exports = Home;
