4da2cc81cfc13a1a033454a3057751f8d6e9e726
Nick Teleky <nteleky@gmail.com>
2015-03-31 06:04:30 -0400

    review list no longer hardcoded; gets reviews based on current class being shown

---
+++ js/src/components/review_list.js
@@ -0,0 +1,54 @@
+//Component for listing all reviews of a given class.
+
+var React = require('react'),
+    mui   = require('material-ui'),
+    $     = require('jquery');
+
+var ReviewList = React.createClass({
+  //  mixins: [ReactRouter.State],
+
+    getReviewsForClass: function(classID) {
+        var response = $.ajax({
+            url: 'api/review',
+            method: 'POST',
+            data: {
+                method: 'list_reviews',
+                class_id: classID
+            },
+            dataType: 'json',
+            async: false
+        }).responseText;
+        var data = JSON.parse(response);
+        //TODO - add "if not successful" clause here.
+        return data.Return;
+    },
+
+    render: function() {
+        var uri_params = this.getParams();
+        var reviews = this.getReviewsForClass(uri_params.classId);
+        //TODO - if no reviews, say so!
+        console.log(reviews);
+
+        return (
+            <div className="review-list-component">
+                <h4>Review List</h4>
+                <ul>{reviews.map(function(review, i) {
+                        return (
+                            <div className="review-list-review">
+                                <mui.Paper zDepth={1}>
+                                    <div className="review-title">Title: {review.Title}</div>
+                                    <div className="review-date">Date: {review.Date}</div>
+                                    <div className="review-text">Review: {review.Review}</div>
+                                    <div className="review-recommend"> Recommend: {review.Recommend.toString()}</div>
+                                </mui.Paper>
+                            </div>
+                            );
+                     })}
+                </ul>
+            </div>
+        );
+    }
+});
+
+module.exports = ReviewList;
+
